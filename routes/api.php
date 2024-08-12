64f8f8727b0ec0b51e65bd1f28808698ac35bb0e
vicsystems <victorasuquob@gmail.com>
2024-08-12 03:37:14 +0100

    api setup

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\API\v1\ApiAuthController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +15,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group([
+  'prefix' => '/v1',
+],
+function () {
+
+  // Route::get('test-route', [ApiAuthController::class,'register']);
+
+  Route::post('/register', [ApiAuthController::class, 'register']);
+
+  Route::post('/login', [ApiAuthController::class, 'login']);
+
+  Route::post('/verify-otp', [ApiAuthController::class, 'verify_otp']);
+
+  Route::post('/resend-otp', [ApiAuthController::class, 'resend_otp']);
+
 });
