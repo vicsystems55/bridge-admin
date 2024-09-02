0ebf52e2b9ae3c9c02c0019bbc4d38eb94527e58
vicsystems <victorasuquob@gmail.com>
2024-09-03 00:17:37 +0100

    company profile update

--- app/Models/CompanyProfile.php
+++ app/Models/CompanyProfile.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class CompanyProfile extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
 }
--- app/Models/RecruiterProfile.php
+++ app/Models/RecruiterProfile.php
@@ -8,4 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class RecruiterProfile extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
 }
