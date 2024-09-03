513f4c5a522866f039e445b0073d5cf081fa70d1
vicsystems <victorasuquob@gmail.com>
2024-09-03 03:28:21 +0100

    recruiter module

--- app/Http/Controllers/RecruiterProfileController.php
+++ app/Http/Controllers/RecruiterProfileController.php
@@ -21,23 +21,9 @@ class RecruiterProfileController extends Controller
     public function store(Request $request)
     {
         //
+        
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(RecruiterProfile $recruiterProfile)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, RecruiterProfile $recruiterProfile)
-    {
-        //
-    }
 
     /**
      * Remove the specified resource from storage.
