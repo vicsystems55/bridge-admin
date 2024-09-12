dafda9bf258713dfb7f22641cd5bd359f6b87d5f
vicsystems <victorasuquob@gmail.com>
2024-09-12 13:50:32 +0100

    job seeker fetch

--- app/Http/Controllers/UserPermissionsController.php
+++ app/Http/Controllers/UserPermissionsController.php
@@ -12,55 +12,55 @@ use Spatie\Permission\Models\Permission;
 
 class UserPermissionsController extends Controller
 {
-    //
+  //
 
-    public function create(Request $request){
-
-
-      $role = Role::create(['name' =>$request->role_name]);
-
-      return $role;
+  public function create(Request $request)
+  {
 
 
-    }
+    $role = Role::create(['name' => $request->role_name]);
 
-    public function user_roles(Request $request){
+    return $role;
+  }
 
-      return $request->user()->id;
+  public function user_roles(Request $request)
+  {
 
-      return User::with('roles')->find(request()->id);
+    return $request->user()->id;
 
-    }
+    return User::with('roles')->find(request()->id);
+  }
 
-    public function assign_role(Request $request){
+  public function assign_role(Request $request)
+  {
 
-      $user = User::find($request->user()->id);
-      $user->assignRole($request->role_name);
+    $user = User::find($request->user()->id);
+    $user->assignRole($request->role_name);
 
-      return User::with('roles')->find($request->user()->id);
+    return User::with('roles')->find($request->user()->id);
+  }
 
+  public function jobSeekers(Request $request)
+  {
 
-
-    }
-
-    public function jobSeekers(Request $request){
-
-      $users = User::with([
-        'profile',
-        'work_experiences',
-        'education',
-        'skills',
-        'resume',
-        'languages'
-        ])->whereHas('roles', function ($query) {
-        $query->where('name', 'job_seeker');
+    $users = User::with([
+      'profile',
+      'work_experiences',
+      'education',
+      'skills',
+      'resume',
+      'languages'
+    ])->whereHas('roles', function ($query) {
+      $query->where('name', 'job_seeker');
     })->whereHas('profile', function ($query) {
       // Ensures that only users with a profile are retrieved.
       $query->whereNotNull('id'); // Assuming 'id' is a required field in profile; adjust as needed.
-  })
-    ->get();
+    })->whereHas('education', function ($query) {
+      // Ensures that only users with a profile are retrieved.
+      $query->whereNotNull('id'); // Assuming 'id' is a required field in profile; adjust as needed.
+    })
+      ->get();
 
     return $users;
-
-    }
+  }
 }
