27bc2f41f77ef28c1ad65f3e8cfc526436ac2859
vicsystems <victorasuquob@gmail.com>
2024-10-07 16:18:54 +0100

    notifications page

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -2,13 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Notification;
 use Illuminate\Http\Request;
 
 class NotificationController extends Controller
 {
     //
 
-    public function index(){
-      return 123;
+    public function index(Request $request){
+
+
+      return Notification::where('user_id', $request->user()->id)->latest()->get();
     }
 }
