9cdbb658cfeb2e413a83367a2a9c32fe5def9319
vicsystems <victorasuquob@gmail.com>
2025-01-29 20:54:26 +0100

    project Search controller

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -103,7 +103,7 @@ class ProjectController extends Controller
 
       // Apply industry filter if selected
       if (!empty($selectedIndustry)) {
-          $projects->whereIn('categr', $selectedIndustry);
+          $projects->whereIn('category', $selectedIndustry);
       }
 
 
